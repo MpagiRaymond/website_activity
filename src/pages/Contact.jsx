46437bd8c1c4fabcc868024e04010db8413cdfba
MpagiRaymond <raympagi1@gmail.com>
2025-02-19 09:59:50 +0300

    website activity

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,20 +4,25 @@ import './Contact.css'
 const Contact = () => {
   const [name, setName] =  useState('')
   const [email, setEmail] = useState('')
+  const [pwd, setPwd] = useState('')
 
-  const handleSubmit = () => {
-    setName(name);
-    setEmail(email);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(`Name:${name}, Email: ${email}, Password:${pwd}`)
   }
 
   return (
-    <div style={{display:"flex", flexDirection: "column"}}>
+    <div className="dflex">
     <h1>Sign in</h1>
     <form onSubmit={handleSubmit}>
       {/* <label>Name</label> */}
-      <input type="text" name="name" placeholder="Name"/>
+      <input type="text" name="name" onChange={()=> setName(name)} placeholder="Name"/>
       {/* <label>Email</label> */}
-      <input type="email" name="email" placeholder="Email" />
+      <input type="email" name="email" onChange={()=> setEmail(email)} placeholder="Email" />
+      <div className="dflex drow pass">
+        <input type="password" name="password" onChange={()=> setPwd(pwd)} placeholder="Password" />
+        <input type="checkbox" name="viewpwd"  />
+      </div>
       {/* <label>Message</label> */}
       <textarea name="message" placeholder='Message'></textarea>
       <button type="submit">SUBMIT</button>
